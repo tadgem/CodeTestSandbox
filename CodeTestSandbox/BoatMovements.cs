b20167208cff3490c478e6f8cb966374b44eaf54
Tadge <98434757+tadgem@users.noreply.github.com>
2025-03-17 22:27:38 +0000

    Initial commit

---
+++ CodeTestSandbox/BoatMovements.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public class BoatMovements
+{
+    public static bool CanTravelTo(bool[,] gameMatrix, int fromRow, int fromColumn, int toRow, int toColumn)
+    {
+        // Bounds checking
+        if (toColumn < 0 || toRow < 0)
+        {
+            return false;
+        }
+        if (toColumn >= gameMatrix.GetLength(0) || toRow >= gameMatrix.GetLength(1))
+        {
+            return false;
+        }
+        if (fromColumn < 0 || fromRow < 0)
+        {
+            return false;
+        }
+        if (fromColumn >= gameMatrix.GetLength(0) || fromRow >= gameMatrix.GetLength(1))
+        {
+            return false;
+        }
+        int rowDist = Math.Abs(toRow - fromRow);
+        int columnDist = Math.Abs(toColumn - fromColumn);
+
+        if (rowDist > 1 || columnDist > 2)
+        {
+            return false;
+        }
+        if (rowDist > 0 && columnDist > 0)
+        {
+            return false;
+        }
+
+        if (columnDist == 2 && toColumn < fromColumn)
+        {
+            return false;
+        }
+        if (columnDist == 2)
+        {
+            if (!gameMatrix[fromRow, fromColumn + 1])
+            {
+                return false;
+            }
+        }
+        return gameMatrix[toRow, toColumn];
+    }
+
+    //public static void Main()
+    //{
+    //    bool[,] gameMatrix =
+    //    {
+    //        {false, true,  true,  false, false, false},
+    //        {true,  true,  true,  false, false, false},
+    //        {true,  true,  true,  true,  true,  true},
+    //        {false, true,  true,  false, true,  true},
+    //        {false, true,  true,  true,  false, true},
+    //        {false, false, false, false, false, false},
+    //    };
+
+    //    Console.WriteLine(CanTravelTo(gameMatrix, 3, 2, 2, 2)); // true, Valid move
+    //    Console.WriteLine(CanTravelTo(gameMatrix, 3, 2, 3, 4)); // false, Can't travel through land
+    //    Console.WriteLine(CanTravelTo(gameMatrix, 3, 2, 6, 2)); // false, Out of bounds
+    //}
+}
---
+++ CodeTestSandbox/RoutePlanner.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+public class RoutePlanner
+{
+    public struct Coordinate
+    {
+        public int row, col;
+
+        public Coordinate(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Coordinate c)
+            {
+                return row == c.row && col == c.col;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"R:{row} C:{col}";
+        }
+
+        public override int GetHashCode()
+        {
+            return row.GetHashCode() ^ col.GetHashCode();
+        }
+    }
+
+    public static bool CoordinateInBoundsAndTraversible(Coordinate c, bool[,] map)
+    {
+        // bounds checks
+        if (c.row < 0 || c.row >= map.GetLength(0))
+        {
+            return false;
+        }
+        if (c.col < 0 || c.col >= map.GetLength(1))
+        {
+            return false;
+        }
+        return map[c.row, c.col];
+    }
+
+    public static bool RecurseRoutePossible(Coordinate current, Coordinate to, bool[,] map, bool[,] visited)
+    {
+        if (current.Equals(to)) return true;
+        visited[current.row, current.col] = true;
+
+        Coordinate down = current;
+        down.row += 1;
+
+        Coordinate right = current;
+        right.col += 1;
+
+        Coordinate up = current;
+        up.row -= 1;
+
+        Coordinate left = current;
+        left.col -= 1;
+
+        bool canMoveDown = CoordinateInBoundsAndTraversible(down, map);
+        bool canMoveRight = CoordinateInBoundsAndTraversible(right, map);
+        bool canMoveUp = CoordinateInBoundsAndTraversible(up, map);
+        bool canMoveLeft = CoordinateInBoundsAndTraversible(left, map);
+
+        bool result = false;
+        if (canMoveDown && !visited[down.row, down.col])
+        {
+            result = result || RecurseRoutePossible(down, to, map, visited);
+        }
+        if (canMoveRight && !visited[right.row, right.col])
+        {
+            result = result || RecurseRoutePossible(right, to, map, visited);
+        }
+        if (canMoveLeft && !visited[left.row, left.col])
+        {
+            result = result || RecurseRoutePossible(left, to, map, visited);
+        }
+        if (canMoveUp && !visited[up.row, up.col])
+        {
+            result = result || RecurseRoutePossible(up, to, map, visited);
+        }
+
+        return result;
+    }
+
+    public static bool RouteExists(int fromRow, int fromColumn, int toRow, int toColumn,
+                                  bool[,] mapMatrix)
+    {
+        Coordinate from = new Coordinate(fromRow, fromColumn);
+        Coordinate to = new Coordinate(toRow, toColumn);
+        bool[,] visited = new bool[mapMatrix.GetLength(0), mapMatrix.GetLength(1)];
+
+        return RecurseRoutePossible(from, to, mapMatrix, visited);
+    }
+
+    public static void Main(string[] args)
+    {
+        bool[,] mapMatrix = {
+            {true, false, false},
+            {true, true, false},
+            {false, true, true}
+        };
+
+        Console.WriteLine(RouteExists(0, 0, 2, 2, mapMatrix));
+    }
+}
